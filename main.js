cda93fad75ac1fd03b6bf3a7f1c92cf2cbc40911
Kate Park <rudhi1009@gmail.com>
2019-04-06 02:58:32 -0700

    songs added and reset cards

--- main.js
+++ main.js
@@ -5,21 +5,25 @@ function initializeApp(){
     $('.remote').hide();
     makeCard();
     display_stats();
-    $('.card').on('click','.back',card_clicked);
     $('.gamestats').on('click','.reset', reset_game);
+    $('.tvcontainer').on('click', '.speakerbtn', function(){
+        toggle_sound();
+        $('.speakerbtn').toggleClass("mutebtn");
+    });
 }
-
 var first_card_clicked = null;
 var first_card = null;
 var second_card_clicked = null;
 var second_card = null;
-var total_possible_matches = 9; //total possible matches, 카드 4장일때는 2번
+var total_possible_matches = 9;
 var match_counter = 0;
 var matches = 0;
 var attempts = 0;
 var accuracy = 0;
 var games_played = 0;
 var noClickable = false;
+var player = null;
+var mute = false;
 
 var frontimages = [
     "images/akmu2.png",
@@ -53,6 +57,17 @@ var gifs = [
     "images/snsdgif.gif",
     "images/akmugif.gif"
 ];
+var musicSrc = [
+    "songs/akmu.mp3",
+    "songs/blackpink.mp3",
+    "songs/bts.mp3",
+    'songs/exo.mp3',
+    "songs/iu.mp3",
+    "songs/mamamoo.mp3",
+    "songs/redvelvet.mp3",
+    "songs/snsd.mp3",
+    "songs/twice.mp3"
+]
 
 var suffleImages= []; //check
 
@@ -68,6 +83,7 @@ function display_stats () {
 }
 
 function makeCard(){
+    $('.row1').empty();
     suffleCards(frontimages); 
     for(var i = 0 ; i < 18 ; i++ ) {
         var row1 = $('.row1');
@@ -85,6 +101,7 @@ function makeCard(){
         $(cardcontainer).append(card);
         row1.append(cardcontainer);    
     }
+    $('.card').on('click','.back',card_clicked);
 }
     
 function suffleCards(a) {
@@ -94,7 +111,6 @@ function suffleCards(a) {
     }
     suffleImages = a; 
 }
-    
 
 function card_clicked(){
     if(games_played === 0) {
@@ -104,6 +120,7 @@ function card_clicked(){
         return;
     }
     if(first_card_clicked === null){
+
         $(this).hide();
         first_card = $(this);
         first_card_clicked = $(this).prev().find(".frontimage").attr("src");
@@ -111,36 +128,46 @@ function card_clicked(){
         second_card = $(this);
         second_card_clicked = $(this).prev().find(".frontimage").attr("src");
         if(first_card_clicked === second_card_clicked) {
+            $('.second_card').css("transform", "rotateY( 180deg )");
             $('.remote').fadeIn(700);
             $('.remote').fadeOut();
             match_counter++;
             var gif=null;
             switch (second_card_clicked) {
                 case "images/akmu2.png":
+                playSound(musicSrc[0]);
                 gif = gifs[8];
                 break;
                 case "images/blackpink22.png":
+                playSound(musicSrc[1]);
                 gif = gifs[1];
                 break;
                 case "images/iu32.png":
+                playSound(musicSrc[4]);
                 gif = gifs[0];
                 break;
                 case "images/mamamoo2.png":
+                playSound(musicSrc[5]);
                 gif = gifs[5];
                 break;
                 case "images/redvelvet3.png":
+                playSound(musicSrc[6]);
                 gif = gifs[6];
                 break;
                 case "images/snsd22.png":
+                playSound(musicSrc[7]);
                 gif = gifs[7];
                 break;
                 case "images/twice23.png":
+                playSound(musicSrc[8]);
                 gif = gifs[4];
                 break;
                 case "images/bts2.png":
+                playSound(musicSrc[2]);
                 gif = gifs[3];
                 break;
                 case "images/exo2 (2).png":
+                playSound(musicSrc[3]);
                 gif = gifs[2];
                 break;
             }
@@ -148,18 +175,17 @@ function card_clicked(){
             if(match_counter === total_possible_matches) {
                 $('.modal').show();
                 $('.attemptresult').text(attempts+1);
-                $('.accuracyresult').text(accuracy);
+                $('.accuracyresult').text(Math.floor(match_counter / (attempts +1 ) * 100) + "%");
                 $('.modal').on('click','.retrybtn',function(){
                     reset_game();
                     $('.modal').hide();
                 });
             }
-            
             matches++;
             attempts++;
             $(this).hide();
-            first_card.parent().css({"background-color":"#a29bfe"})
-            second_card.parent().css({"background-color":"#a29bfe"})
+            first_card.parent().css({"background-image":"url('images/purple.gif')"})
+            second_card.parent().css({"background-image":"url('images/purple.gif')"})
             first_card_clicked = null;
             second_card_clicked = null;        
         } else {
@@ -189,11 +215,35 @@ function reset_stats() {
 function reset_game(){
     games_played++;
     match_counter = 0;
-    suffleCards(frontimages); 
+    console.log('suffleimg',suffleImages); //front이미지 지우고 다시 만들기
+    suffleCards(suffleImages); 
     reset_stats();
     display_stats();
     $('.back').show();
-    $('.card').css({"background-color":"#f9ca24"})
+    // $('.card').css({"background-color":"#f9ca24"});
+    $('.card').css({"background-image":'url("images/yellow.gif")'});
     $('.tvcover').removeAttr('style');
     $('.accuracy .value').text("0 %");
+    makeCard();
+    player.muted = true;
+}
+
+function playSound(src) {
+    if (player) {
+        player.pause();
+    }
+    player = new Audio(src);
+    player.volume = .2;
+    if (!mute) {
+        player.play();
+    }
+}
+
+function toggle_sound(){
+    $('.speakerbtn').css("background-img","url('images/white-speaker-muted-hi.png')");
+    mute = !mute;
+    player.muted = mute;
+    if (!mute) {
+        player.play();
+    }
 }
